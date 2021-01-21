8265b74f2f1a0b8b965107b4bbd7e427a26a9315
11471 <11471@windows10.microdone.cn>
2021-01-21 11:26:04 +0800

    d

---
+++ src/main/java/com/tomulist/demo/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.tomulist.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/")
+	public String homePage() {
+		
+		return "index";
+	}
+}
